4d9bf683d0a16e08e1b7619e156117da57abd6e0
Corey Martin <coreym@gmail.com>
2015-09-10 15:34:59 -0400

    rename test

--- spec/features/feed_spec.rb
+++ spec/features/feed_spec.rb
@@ -1,19 +1,19 @@
-require 'spec_helper'
+require "spec_helper"
 
-context 'External request' do
+context "External request" do
 
   before :each do 
     redis = Redis.new
     redis.flushall
   end
 
-  it 'returns RSS articles from a feed' do
+  it "returns RSS articles from a feed" do
     get "/v1/feed?url=http://fakedomain.com/feed.xml"
     json = JSON.parse(last_response.body)
     expect(json["items"].count > 0).to eq true
   end
 
-  it 'returns the proper structure' do
+  it "returns the proper structure" do
     get "/v1/feed?url=http://fakedomain.com/feed.xml"
     json = JSON.parse(last_response.body)
     item = json["items"][0]
@@ -22,13 +22,13 @@ context 'External request' do
     expect(item["link"]).not_to eq nil
   end
 
-  it 'returns the proper count' do
+  it "returns the proper count" do
     get "/v1/feed?url=http://fakedomain.com/feed.xml&count=5"
     json = JSON.parse(last_response.body)
     expect(json["items"].count).to eq 5
   end
 
-  it 'uses the cache for subsequent requests' do
+  it "uses the cache for subsequent requests" do
     get "/v1/feed?url=http://fakedomain.com/feed.xml&count=5"
     json = JSON.parse(last_response.body)
     expect(json["from_proxy_cache"]).to eq false
